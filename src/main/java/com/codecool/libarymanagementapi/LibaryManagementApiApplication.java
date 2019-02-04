e9de181dcc1345ae60215391df66cfc293720384
Uj Levente <asdlevente@gmail.com>
2019-02-04 10:30:44 +0100

    set up

---
+++ src/main/java/com/codecool/libarymanagementapi/LibaryManagementApiApplication.java
@@ -0,0 +1,14 @@
+package com.codecool.libarymanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibaryManagementApiApplication.class, args);
+    }
+
+}
+
